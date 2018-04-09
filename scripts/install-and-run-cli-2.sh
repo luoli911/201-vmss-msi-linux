92151b3616e7bc0a2113befd8211eea55c99dd55
luoli911 <v-liiluo@microsoft.com>
2018-04-09 16:56:05 +0800

    Update install-and-run-cli-2.sh

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -37,58 +37,40 @@ do
 
 done
 
-# Install Docker and then run docker image with cli
+# Install Azure CLI 
 
-sudo apt-get -y update
-sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get -y update
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get -y update
-sudo apt-get -y install docker-ce
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+     
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
 
 sudo apt-get -y update
 sudo apt-get install cifs-utils
 
-
 today=$(date +%Y-%m-%d)
 currenttime=$(date +%s)
 machineName=$(hostname)
+scenario2="BuildTask"
 sudo mkdir /mnt/azurefiles
 sudo mount -t cifs //acrtestlogs.file.core.windows.net/logshare /mnt/azurefiles -o vers=3.0,username=acrtestlogs,password=ZIisPCN0UrjLfhv6Njiz0Q8w9YizeQgIm6+DIfMtjak4RJrRlzJFn4EcwDUhNvXmmDv5Axw9yGePh3vn1ak8cg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+
 sudo mkdir /mnt/azurefiles/$today
-sudo mkdir /mnt/azurefiles/$today/$machineName$currenttime
+sudo mkdir /mnt/azurefiles/$today/Scenario2
+sudo mkdir /mnt/azurefiles/$today/Scenario2/$machineName$currenttime
 
-sudo systemctl stop docker
-sudo mkdir /etc/systemd/system/docker.service.d
-sudo touch /etc/systemd/system/docker.service.d/docker.conf
-echo -e [Service]\\nExecStart=\\nExecStart=/usr/bin/dockerd --graph=\"/mnt/new_volume\" --storage-driver=aufs |sudo tee /etc/systemd/system/docker.service.d/docker.conf
-sudo systemctl daemon-reload
-sudo systemctl start docker
+echo "---create build task---"
+buildbegin=$(date +%s%3N)
+BuildStartTime=$(date +%H:%M:%S)
+for i in `seq 1 100`
+  do
+    az acr build-task create  --name buildhelloworld$machineName$i  -r acrbuildrg2  -t helloworld$i:v1  --context https://github.com/luoli911/aci-helloworld  --git-access-token 2d218102827a740f05d07b1dcc4fcc6844afb2a4
+  done
+buildend=$(date +%s%3N)
+BuildEndTime=$(date +%H:%M:%S)
+buildtasktime=$((buildend-buildbegin))
+echo registry,region,starttime,endtime,buildtasktime:acrbuildrg2,eastus,$BuildStartTime,$BuildEndTime,$buildtasktime >> /mnt/azurefiles/$today/Scenario2/$machineName$currenttime/buildtask-output.log
 
-sleep 10
 
-echo "---docker pull dotnet from eus.mcr.microsoft.com---"
-pullbegin=$(date +%s%3N)
-PullStartTime=$(date +%H:%M:%S)
-sudo docker pull eus.mcr.microsoft.com/dotnet
-pullend=$(date +%s%3N)
-PullEndTime=$(date +%H:%M:%S)
-pulltime=$((pullend-pullbegin))
-echo "---nslookup eus.mcr.microsoft.com---"
-nslookup=$(nslookup eus.mcr.microsoft.com)
-echo registry,image,region,starttime,endtime,pulltime:eus.mcr.microsoft.com,dotnet,eastus,$PullStartTime,$PullEndTime,$pulltime >> /mnt/azurefiles/$today/$machineName$currenttime/mcr-output.log
-echo $nslookup >> /mnt/azurefiles/$today/$machineName$currenttime/mcr-output.log
-
-#function stresstest()
-#{
-#START=`date +%s`
-#while [ $(( $(date +%s) - 1800 )) -lt $START ]; do
-#    echo "---docker pull dotnet from eus.mcr.microsoft.com---"
-#    sudo docker pull eus.mcr.microsoft.com/dotnet
-#done
-#}
-# stress test and concurrent pull other image 
-#stresstest 
-#wait
