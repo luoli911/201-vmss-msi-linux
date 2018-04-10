525b6437408ec648400f3f4d810fb6335a741eb1
luoli911 <v-liiluo@microsoft.com>
2018-04-10 14:32:54 +0800

    Update install-and-run-cli-2.sh

--- scripts/install-and-run-cli-2.sh
+++ scripts/install-and-run-cli-2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #startuptime1=$(date +%s%3N)
 
-while getopts ":i:a:c:r:" opt; do
+while getopts ":i:a:c:r:p:" opt; do
+
   case $opt in
     i) docker_image="$OPTARG"
     ;;
@@ -11,9 +12,10 @@ while getopts ":i:a:c:r:" opt; do
     ;;
     r) resource_group="$OPTARG"
     ;;
-    p) port="$OPTARG"
+    p) password="$OPTARG"
     ;;
-    t) script_file="$OPTARG"
+
+t) script_file="$OPTARG"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     ;;
@@ -48,9 +50,18 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get -y update
 sudo apt-get -y install docker-ce
 
-sudo apt-get -y update
-sudo apt-get install cifs-utils
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get install -y apt-transport-https
+sudo apt-get -y update && sudo apt-get install -y azure-cli
 
+sudo apt-get -y update
+sudo apt-get install -y cifs-utils
+
+sudo apt-get -y update
+sudo apt install -y git-all
 
 today=$(date +%Y-%m-%d)
 currenttime=$(date +%s)
@@ -58,7 +69,8 @@ machineName=$(hostname)
 sudo mkdir /mnt/azurefiles
 sudo mount -t cifs //acrtestlogs.file.core.windows.net/logshare /mnt/azurefiles -o vers=3.0,username=acrtestlogs,password=ZIisPCN0UrjLfhv6Njiz0Q8w9YizeQgIm6+DIfMtjak4RJrRlzJFn4EcwDUhNvXmmDv5Axw9yGePh3vn1ak8cg==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 sudo mkdir /mnt/azurefiles/$today
-sudo mkdir /mnt/azurefiles/$today/$machineName$currenttime
+sudo mkdir /mnt/azurefiles/$today/Scenario2
+sudo mkdir /mnt/azurefiles/$today/Scenario2/$machineName$currenttime
 
 sudo systemctl stop docker
 sudo mkdir /etc/systemd/system/docker.service.d
@@ -68,27 +80,27 @@ sudo systemctl daemon-reload
 sudo systemctl start docker
 
 sleep 10
+function loadTest2()
+{
+ACR_NAME="NewACRLoadTestBuildCR"
+az login -u azcrci@microsoft.com -p LPrxL4cW3II8B8Hf76Uz/A==
+az account set --subscription "c451bd61-44a6-4b44-890c-ef4c903b7b12"
+az extension remove -n acrbuildext
+az extension add --source https://acrbuild.blob.core.windows.net/cli/acrbuildext-0.0.2-py2.py3-none-any.whl -y
+az acr login -n $ACR_NAME
 
-echo "---docker pull dotnet from eus.mcr.microsoft.com---"
-pullbegin=$(date +%s%3N)
-PullStartTime=$(date +%H:%M:%S)
-sudo docker pull eus.mcr.microsoft.com/dotnet
-pullend=$(date +%s%3N)
-PullEndTime=$(date +%H:%M:%S)
-pulltime=$((pullend-pullbegin))
-echo "---nslookup eus.mcr.microsoft.com---"
-nslookup=$(nslookup eus.mcr.microsoft.com)
-echo registry,image,region,starttime,endtime,pulltime:eus.mcr.microsoft.com,dotnet,eastus,$PullStartTime,$PullEndTime,$pulltime >> /mnt/azurefiles/$today/$machineName$currenttime/mcr-output.log
-echo $nslookup >> /mnt/azurefiles/$today/$machineName$currenttime/mcr-output.log
+echo "---create build task---"
+buildbegin=$(date +%s%3N)
+BuildStartTime=$(date +%H:%M:%S)
+for i in `seq 1 100`
+  do
+    az acr build-task create  --name buildhelloworld$machineName$i  -r $ACR_NAME  -t helloworld$i:v1  --context https://github.com/luoli911/aci-helloworld  --git-access-token 2d218102827a740f05d07b1dcc4fcc6844afb2a4
+  done
+buildend=$(date +%s%3N)
+BuildEndTime=$(date +%H:%M:%S)
+buildtasktime=$((buildend-buildbegin))
+echo machineName,starttime,endtime,buildtasktime:$machineName,$BuildStartTime,$BuildEndTime,$buildtasktime >> /mnt/azurefiles/$today/Scenario2/acr-buildtask-output-time.log
 
-#function stresstest()
-#{
-#START=`date +%s`
-#while [ $(( $(date +%s) - 1800 )) -lt $START ]; do
-#    echo "---docker pull dotnet from eus.mcr.microsoft.com---"
-#    sudo docker pull eus.mcr.microsoft.com/dotnet
-#done
-#}
-# stress test and concurrent pull other image 
-#stresstest 
-#wait
+}
+
+loadTest2 >> /mnt/azurefiles/$today/Scenario2/$machineName$currenttime/acr-buildtask-output.log
